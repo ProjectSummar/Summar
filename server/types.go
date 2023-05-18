f9c9e540c45811cccdbd1eda6dc409847e69e0c2
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-05-18 13:35:53 +0800

    add user auth session struct

--- server/types.go
+++ server/types.go
@@ -12,6 +12,12 @@ type User struct {
 	CreatedAt time.Time `json:"createdAt"`
 }
 
+type Session struct {
+	Token     string    `json:"token"`
+	UserID    uuid.UUID `json:"userId"`
+	ExpiresAt time.Time `json:"expiresAt"`
+}
+
 type Bookmark struct {
 	ID      string    `json:"id"`
 	UserID  uuid.UUID `json:"userId"`
