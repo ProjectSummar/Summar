7567e13dfdcf8ec60d8ad8ffb5296e3c21cd888f
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-05-21 11:09:54 +0800

    create a session when logging in

--- server/api.go
+++ server/api.go
@@ -76,7 +76,20 @@ func (s *APIServer) LoginHandler(w http.ResponseWriter, r *http.Request) error {
 	}
 
 	// create a session
+	sessionToken, err := CreateSessionToken()
+	if err != nil {
+		return err
+	}
+
+	session := NewSession(sessionToken, user.ID)
+
+	if err := s.Db.CreateSession(&session); err != nil {
+		return err
+	}
+
 	// return session token to be stored as cookie
+	SetSessionTokenCookie(w, sessionToken)
+
 	WriteJSON(w, http.StatusOK, LoginResponse{
 		APIResponse: APIResponse{
 			IsOk: true,
