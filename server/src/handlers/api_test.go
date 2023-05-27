94db4ea0e8094702aa5725eb47fc47ab89574551
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-05-27 14:20:57 +0800

    add get user handler test

--- server/src/handlers/api_test.go
+++ server/src/handlers/api_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -8,6 +9,8 @@ import (
 	"summar/server/stores"
 	"summar/server/utils"
 	"testing"
+
+	"github.com/google/uuid"
 )
 
 func TestHandlers(t *testing.T) {
@@ -16,6 +19,8 @@ func TestHandlers(t *testing.T) {
 	handlers := Handlers{
 		Store: mockStore,
 	}
+
+	var userId uuid.UUID
 
 	t.Run("Test signup handler", func(t *testing.T) {
 		reqBody := utils.JSONMarshal(&SignupRequest{
@@ -42,6 +47,10 @@ func TestHandlers(t *testing.T) {
 		if err := json.Unmarshal(rr.Body.Bytes(), &resBody); err != nil {
 			t.Fatal(err)
 		}
+
+		t.Logf("signup response:\n%+v\n", resBody)
+
+		userId = resBody.User.Id
 	})
 
 	t.Run("Test login handler", func(t *testing.T) {
@@ -69,5 +78,33 @@ func TestHandlers(t *testing.T) {
 		if err := json.Unmarshal(rr.Body.Bytes(), &resBody); err != nil {
 			t.Fatal(err)
 		}
+
+		t.Logf("login response:\n%+v\n", resBody)
+	})
+
+	t.Run("Test get user handler", func(t *testing.T) {
+		req, err := http.NewRequest("POST", "/login", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		ctx := context.WithValue(req.Context(), "userId", userId)
+		req = req.WithContext(ctx)
+
+		rr := httptest.NewRecorder()
+
+		handler := ToHttpHandlerFunc(handlers.GetUserHandler)
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("Handler errored out: got %v want %v", status, http.StatusOK)
+		}
+
+		var resBody GetUserResponse
+		if err := json.Unmarshal(rr.Body.Bytes(), &resBody); err != nil {
+			t.Fatal(err)
+		}
+
+		t.Logf("get user response:\n%+v\n", resBody)
 	})
 }
