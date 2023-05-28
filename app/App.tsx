89a695a36a2c43f7a517cc4086dbb5aba712725a
Soqoro <qirong36@gmail.com>
2023-05-28 19:41:36 +0800

    minor modification

--- app/App.tsx
+++ app/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world baby!</Text>
+      <Text>Hello world baby!!@!</Text>
       <StatusBar style="auto" />
     </View>
   );
