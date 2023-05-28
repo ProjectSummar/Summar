be77a347e607aecd51ceef1bb73f9797ad4872e1
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-05-28 11:34:31 +0800

    JSON unmarshal util

--- server/src/utils/utils.go
+++ server/src/utils/utils.go
@@ -1,6 +1,8 @@
 package utils
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
 
 func JSONMarshalIndent(v interface{}) string {
 	s, _ := json.MarshalIndent(v, "", "\t")
@@ -10,6 +12,16 @@ func JSONMarshalIndent(v interface{}) string {
 func JSONMarshal(v interface{}) string {
 	s, _ := json.Marshal(v)
 	return string(s)
+}
+
+func JSONUnmarshal[T any](body []byte) (*T, error) {
+	var resBody T
+
+	if err := json.Unmarshal(body, &resBody); err != nil {
+		return nil, err
+	} else {
+		return &resBody, nil
+	}
 }
 
 func StringPtr(s string) *string {
