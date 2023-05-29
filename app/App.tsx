5f37512f6d5ef2e56be978775840206b39a0cb8c
Soqoro <qirong36@gmail.com>
2023-05-29 23:29:03 +0800

    Sign in page v1

--- app/App.tsx
+++ app/App.tsx
@@ -1,20 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import SignInPage from "./screens/SignInPage";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world baby!!@!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return <SignInPage />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
