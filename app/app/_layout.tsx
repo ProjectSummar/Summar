b6c4c711e46c2f3b7f3b2afdaaba4418b4b5507e
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-06-01 23:06:55 +0800

    add queryclient to app

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,7 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 
 const Layout = () => {
-    return <Stack screenOptions={{ headerShown: false }} />;
+    const queryClient = new QueryClient();
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Stack screenOptions={{ headerShown: false }} />
+        </QueryClientProvider>
+    );
 };
 
 export default Layout;
