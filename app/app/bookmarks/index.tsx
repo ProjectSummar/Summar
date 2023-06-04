76f29c0f5ef9210f0f6eb692822844c83e843699
Soqoro <qirong36@gmail.com>
2023-06-04 22:14:03 +0800

    sidebar testing

--- app/app/bookmarks/index.tsx
+++ app/app/bookmarks/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, TextInput, View } from "react-native";
 const Index = () => {
     return (
         <View>
-            <Text>This is the main page</Text>
+            <Text>This is the main page!!!!</Text>
         </View>
     );
 };
