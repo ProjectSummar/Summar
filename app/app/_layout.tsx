a5d5ff71db872ab5d4f6b7e041729853e2ee9c10
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-06-04 21:13:30 +0800

    auth provider

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { AuthContextProvider } from "@src/contexts/auth-context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -8,7 +9,9 @@ const Layout = () => {
     return (
         <SafeAreaProvider>
             <QueryClientProvider client={queryClient}>
-                <Stack screenOptions={{ headerShown: false }} />
+                <AuthContextProvider>
+                    <Stack screenOptions={{ headerShown: false }} />
+                </AuthContextProvider>
             </QueryClientProvider>
         </SafeAreaProvider>
     );
