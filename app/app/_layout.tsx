e9c0b5acdcdaad9b45e7ede59572123f6dde00fb
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-06-05 12:37:14 +0800

    add feature flag for auth context

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -3,18 +3,30 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+const AUTH_CONTEXT = false;
+
 const Layout = () => {
     const queryClient = new QueryClient();
 
-    return (
-        <SafeAreaProvider>
-            <QueryClientProvider client={queryClient}>
-                <AuthContextProvider>
+    if (AUTH_CONTEXT) {
+        return (
+            <SafeAreaProvider>
+                <QueryClientProvider client={queryClient}>
+                    <AuthContextProvider>
+                        <Stack screenOptions={{ headerShown: false }} />
+                    </AuthContextProvider>
+                </QueryClientProvider>
+            </SafeAreaProvider>
+        );
+    } else {
+        return (
+            <SafeAreaProvider>
+                <QueryClientProvider client={queryClient}>
                     <Stack screenOptions={{ headerShown: false }} />
-                </AuthContextProvider>
-            </QueryClientProvider>
-        </SafeAreaProvider>
-    );
+                </QueryClientProvider>
+            </SafeAreaProvider>
+        );
+    }
 };
 
 export default Layout;
