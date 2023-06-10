5277eb002b5acef5bcfd3c7beb29c9a875d9792b
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-06-10 15:05:31 +0800

    create bookmark icon

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,12 +1,39 @@
 import { AuthContextProvider } from "@src/contexts/auth-context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Slot } from "expo-router";
+import { useEffect, useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-const AUTH_CONTEXT = false;
+const AUTH_CONTEXT = true;
 
 const Layout = () => {
+    const [ready, setReady] = useState(false);
+
     const queryClient = new QueryClient();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                SplashScreen.preventAutoHideAsync();
+
+                const fonts = cacheFonts([Ionicons.font]);
+
+                await Promise.all(fonts);
+            } catch (err) {
+                console.warn(err);
+            } finally {
+                setReady(true);
+                SplashScreen.hideAsync();
+            }
+        })();
+    }, []);
+
+    if (!ready) {
+        return null;
+    }
 
     if (AUTH_CONTEXT) {
         return (
@@ -27,6 +54,10 @@ const Layout = () => {
             </SafeAreaProvider>
         );
     }
+};
+
+const cacheFonts = (fonts: any[]) => {
+    return fonts.map((font) => Font.loadAsync(font));
 };
 
 export default Layout;
--- app/app/main/bookmark/index.tsx
+++ app/app/main/bookmark/index.tsx
@@ -1,7 +1,8 @@
+import { Ionicons } from "@expo/vector-icons";
 import { useGetBookmarks } from "@src/api/bookmark";
 import BookmarkCard from "@src/components/bookmark-card";
 import { Link, Stack } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 const Index = () => {
     const { data: bookmarks, isLoading } = useGetBookmarks();
@@ -25,7 +26,7 @@ const Index = () => {
 const CreateButton = () => {
     return (
         <Link href="/main/bookmark/create" asChild>
-            <Button title="+" />
+            <Ionicons name="create-outline" size={30} />
         </Link>
     );
 };
