a827dc53fc84e6fe69dd64e353f9d1e232d16641
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-06-11 23:30:52 +0800

    formatting

--- app/app/main/bookmark/update.tsx
+++ app/app/main/bookmark/update.tsx
@@ -54,7 +54,7 @@ const Update = () => {
                 onSettled: () => {
                     resetForm();
                 },
-            },
+            }
         );
     });
 
--- app/src/components/bookmark-card-context-menu.tsx
+++ app/src/components/bookmark-card-context-menu.tsx
@@ -23,7 +23,7 @@ const BookmarkCardContextMenu = ({ bookmark }: { bookmark: Bookmark }) => {
             {
                 onSuccess: () => successToast("Bookmark deleted successfully"),
                 onError: () => errorToast("Error deleting bookmark"),
-            },
+            }
         );
     };
 
@@ -53,7 +53,8 @@ const BookmarkCardContextMenu = ({ bookmark }: { bookmark: Bookmark }) => {
                         router.push({
                             pathname: "/main/bookmark/update",
                             params: { id: bookmark.id },
-                        })}
+                        })
+                    }
                     icon="md-pencil-sharp"
                 />
             </MenuOptions>
