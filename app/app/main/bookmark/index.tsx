424f82c5e6e33a84f8d8052012e14f75f62d09ed
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-06-11 10:56:03 +0800

    use flatlist

--- app/app/main/bookmark/index.tsx
+++ app/app/main/bookmark/index.tsx
@@ -2,23 +2,22 @@ import { Ionicons } from "@expo/vector-icons";
 import { useGetBookmarks } from "@src/api/bookmark";
 import BookmarkCard from "@src/components/bookmark-card";
 import { Link, Stack } from "expo-router";
-import { Text, View } from "react-native";
+import { FlatList } from "react-native";
 
 const Index = () => {
     const { data: bookmarks, isLoading } = useGetBookmarks();
-
-    if (!bookmarks || isLoading) return <Text>Loading...</Text>;
 
     return (
         <>
             <Stack.Screen
                 options={{ title: "Bookmarks", headerRight: CreateButton }}
             />
-            <View>
-                {bookmarks?.map((bookmark, index) => (
-                    <BookmarkCard key={index} bookmark={bookmark} />
-                ))}
-            </View>
+            <FlatList
+                data={bookmarks}
+                renderItem={({ item }) => <BookmarkCard bookmark={item} />}
+                keyExtractor={(item) => item.id}
+                refreshing={!bookmarks || isLoading}
+            />
         </>
     );
 };
