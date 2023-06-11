e8771e86f337d3cdcf69d9da620673ec4e419cd2
Soqoro <qirong36@gmail.com>
2023-06-11 22:39:13 +0800

    Merged from upstream

---
+++ app/app/bookmarks/DrawerContent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
+
+export function DrawerContent() {
+    return (
+        <View>
+            <Text>Testing</Text>
+        </View>
+    );
+}
