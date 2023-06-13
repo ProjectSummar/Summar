35b845c107c147c964ed644fd311adb90a92f809
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-06-13 16:12:20 +0800

    abstract out context menu

--- app/src/components/bookmark-card-context-menu.tsx
+++ app/src/components/bookmark-card-context-menu.tsx
@@ -1,15 +1,9 @@
-import { Ionicons } from "@expo/vector-icons";
-import {
-    Menu,
-    MenuOption,
-    MenuOptions,
-    MenuTrigger,
-} from "react-native-popup-menu";
-import { Share, Text } from "react-native";
-import { Bookmark, IconName } from "@src/types";
+import { Share } from "react-native";
+import { Bookmark } from "@src/types";
 import { useDeleteBookmark } from "@src/api/bookmark";
 import { useRouter } from "expo-router";
 import { useToast } from "@src/contexts/toast-context";
+import { ContextMenu, ContextMenuOption } from "./context-menu";
 
 const BookmarkCardContextMenu = ({ bookmark }: { bookmark: Bookmark }) => {
     const { errorToast, successToast } = useToast();
@@ -25,7 +19,7 @@ const BookmarkCardContextMenu = ({ bookmark }: { bookmark: Bookmark }) => {
             {
                 onSuccess: () => successToast("Bookmark deleted successfully"),
                 onError: () => errorToast("Error deleting bookmark"),
-            }
+            },
         );
     };
 
@@ -34,67 +28,27 @@ const BookmarkCardContextMenu = ({ bookmark }: { bookmark: Bookmark }) => {
     };
 
     return (
-        <Menu>
-            <MenuTrigger>
-                <Ionicons
-                    name="ellipsis-vertical"
-                    size={20}
-                    style={{ padding: 10 }}
-                />
-            </MenuTrigger>
-
-            <MenuOptions
-                customStyles={{ optionsContainer: { borderRadius: 10 } }}
-            >
-                <ContextMenuOption
-                    text="Delete Bookmark"
-                    onSelect={deleteBookmarkOnSelect}
-                    icon="trash"
-                />
-                <ContextMenuOption
-                    text="Update Bookmark"
-                    onSelect={() =>
-                        router.push({
-                            pathname: "/main/bookmark/update",
-                            params: { id: bookmark.id },
-                        })
-                    }
-                    icon="md-pencil-sharp"
-                />
-                <ContextMenuOption
-                    text="Share Bookmark"
-                    onSelect={shareBookmark}
-                    icon="share-outline"
-                />
-            </MenuOptions>
-        </Menu>
-    );
-};
-
-const ContextMenuOption = ({
-    text,
-    onSelect,
-    icon,
-}: {
-    text: string;
-    onSelect: () => any;
-    icon: IconName;
-}) => {
-    return (
-        <MenuOption
-            onSelect={onSelect}
-            customStyles={{
-                optionWrapper: {
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                    padding: 10,
-                },
-            }}
-        >
-            <Text style={{ fontSize: 15 }}>{text}</Text>
-            <Ionicons name={icon} size={20} />
-        </MenuOption>
+        <ContextMenu>
+            <ContextMenuOption
+                text="Delete Bookmark"
+                onSelect={deleteBookmarkOnSelect}
+                icon="trash"
+            />
+            <ContextMenuOption
+                text="Update Bookmark"
+                onSelect={() =>
+                    router.push({
+                        pathname: "/main/bookmark/update",
+                        params: { id: bookmark.id },
+                    })}
+                icon="md-pencil-sharp"
+            />
+            <ContextMenuOption
+                text="Share Bookmark"
+                onSelect={shareBookmark}
+                icon="share-outline"
+            />
+        </ContextMenu>
     );
 };
 
--- app/src/components/bookmark-page-context-menu.tsx
+++ app/src/components/bookmark-page-context-menu.tsx
@@ -1,15 +1,9 @@
-import { Ionicons } from "@expo/vector-icons";
-import {
-    Menu,
-    MenuOption,
-    MenuOptions,
-    MenuTrigger,
-} from "react-native-popup-menu";
-import { Share, Text } from "react-native";
-import { Bookmark, IconName } from "@src/types";
+import { Share } from "react-native";
+import { Bookmark } from "@src/types";
 import { useToast } from "@src/contexts/toast-context";
 import { Dispatch, SetStateAction } from "react";
 import { useSummariseBookmark } from "@src/api/bookmark";
+import { ContextMenu, ContextMenuOption } from "./context-menu";
 
 const BookmarkPageContextMenu = ({
     bookmark,
@@ -33,7 +27,7 @@ const BookmarkPageContextMenu = ({
                 onSuccess: () =>
                     successToast("Summarised bookmark successfully"),
                 onError: () => errorToast("Error summarising bookmark"),
-            }
+            },
         );
     };
 
@@ -50,62 +44,23 @@ const BookmarkPageContextMenu = ({
     };
 
     return (
-        <Menu>
-            <MenuTrigger>
-                <Ionicons
-                    name="ellipsis-vertical"
-                    size={20}
-                    style={{ padding: 10 }}
-                />
-            </MenuTrigger>
-
-            <MenuOptions
-                customStyles={{ optionsContainer: { borderRadius: 10 } }}
-            >
-                <ContextMenuOption
-                    text="Summarise Bookmark"
-                    onSelect={summariseBookmarkOnPress}
-                    icon="flash-outline"
-                />
-                <ContextMenuOption
-                    text="Toggle Summary View"
-                    onSelect={toggleSummaryView}
-                    icon="book-outline"
-                />
-                <ContextMenuOption
-                    text="Share Bookmark"
-                    onSelect={shareBookmark}
-                    icon="share-outline"
-                />
-            </MenuOptions>
-        </Menu>
-    );
-};
-
-const ContextMenuOption = ({
-    text,
-    onSelect,
-    icon,
-}: {
-    text: string;
-    onSelect: () => any;
-    icon: IconName;
-}) => {
-    return (
-        <MenuOption
-            onSelect={onSelect}
-            customStyles={{
-                optionWrapper: {
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                    padding: 10,
-                },
-            }}
-        >
-            <Text style={{ fontSize: 15 }}>{text}</Text>
-            <Ionicons name={icon} size={20} />
-        </MenuOption>
+        <ContextMenu>
+            <ContextMenuOption
+                text="Summarise Bookmark"
+                onSelect={summariseBookmarkOnPress}
+                icon="flash-outline"
+            />
+            <ContextMenuOption
+                text="Toggle Summary View"
+                onSelect={toggleSummaryView}
+                icon="book-outline"
+            />
+            <ContextMenuOption
+                text="Share Bookmark"
+                onSelect={shareBookmark}
+                icon="share-outline"
+            />
+        </ContextMenu>
     );
 };
 
