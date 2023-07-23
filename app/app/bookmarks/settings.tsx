0cefc734bfed73ab1598680a6e745ae7d4be3cc5
Sua Qi Rong <99239071+Soqoro@users.noreply.github.com>
2023-07-23 21:26:42 +0800

    Add logo (#28)
    * Merged from upstream
    
    * Minor edit
    
    * Add logo

---
+++ app/app/bookmarks/settings.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+export default function Settings() {
+    return (
+        <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+            <Text style={{ fontSize: 16, fontWeight: "700" }}>
+                Settings Screen
+            </Text>
+        </View>
+    );
+}
